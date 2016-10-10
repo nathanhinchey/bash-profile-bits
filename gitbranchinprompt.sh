0f3ecffbd7bd77ff6102ed014ae340ec798e04f8
Nathan Hinchey <nathanhinchey@gmail.com>
2016-10-10 14:27:12 -0400

    Add git branch in prompt script

---
+++ gitbranchinprompt.sh
@@ -0,0 +1,17 @@
+# Git branch in prompt.
+
+git_color() {
+  if [[ $(git status --porcelain 2> /dev/null) ]]; then
+    echo "33m" #yellow
+  else
+    echo "32m" #green
+  fi
+}
+
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\W\[\033[\$(git_color)\]\$(parse_git_branch)\[\033[34m\] $\[\033[00m\] "
+#version with timestamp:
+# export PS1="\W\[\033[94m\] \t\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
