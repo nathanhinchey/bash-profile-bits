37ee719e3470cc2082a7a346049ab147f5fe28a2
Nathan Hinchey <nathanhinchey@gmail.com>
2018-08-21 16:54:09 -0400

    add educational comments to parse_git_branch

--- gitbranchinprompt.sh
+++ gitbranchinprompt.sh
@@ -11,7 +11,12 @@ git_color() {
 }
 
 parse_git_branch() {
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+  # `git branch 2> /dev/null | sed` run sed on git branch, discarding stderr
+  # `sed -e '/^[^*]/d'` deletes lines not beginning with * literal
+  # capture groups are surrouded with escaped parentheses
+  # `/* \(.*\)/` finds a * literal, space literal, then captures the rest
+  # `sed -e 's/toss \(keep\)/(\1)/'` replaces `toss keep` with `(keep)`
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
 export PS1="\W\[\033[\$(git_color)\]\$(parse_git_branch)\[\033[34m\] $\[\033[00m\] "
