819e4ac4d86ea46f00adc7104a140ae66f35ac58
Nathan Hinchey <nathan.hinchey@dailypay.com>
2019-04-05 16:09:03 -0400

    Fix copy_and_print

--- aliases.sh
+++ aliases.sh
@@ -1,1 +1,12 @@
+copy_and_print(){
+  echo "$1" | pbcopy
+  green_on_black='\033[1;40;92m'
+  default='\033[0;39m'
+  echo "echo -e \"${green_on_black}$1${default}\""
+}
+
 alias 80='echo "---------1---------2---------3---------4---------5---------6---------7---------8"'
+alias shrug=$(copy_and_print "¯\\_(ツ)_/¯")
+# alias sdiff=${string_diff}
+# alias rs=
+alias test='echo -e "\e[107m\e[30mASDF"'
